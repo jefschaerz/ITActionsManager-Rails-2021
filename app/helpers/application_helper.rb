c5990f98db2a44e117a61d84cc8eeb903d9f7fcb
jefschaerz <jef.schaerz@bluewin.ch>
2021-02-23 09:04:55 +0000

    Add user's firstname in Nav. Allow only edition of user's  intervention

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,3 @@
 module ApplicationHelper
-    def isAdmin?
-      user_signed_in? && current_user.role ==='admin'
-    end
+
 end
--- app/helpers/interventions_helper.rb
+++ app/helpers/interventions_helper.rb
@@ -1,2 +1,5 @@
 module InterventionsHelper
+    def isUserConnectedIntervention?(intervention)
+      user_signed_in? && current_user.id === intervention.user_id
+    end
 end
